d6d78fa830158918b10f427a5bd9a1e45ea46bde
kupalovz <gohisobim@gmail.com>
2026-05-02 15:20:07 +0500

    main.c changed

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void salom(void)
+{
+  printf("Function?\n");
+}
+
+void chiziq(void)
+{
+  printf("-----------\n");
+}
+
+int main(void)
+{
+  chiziq();
+  salom();
+  chiziq();
+  return 0;
+}
