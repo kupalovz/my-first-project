5dd45b572f77e06620dc5ad2c2433e036b42cec2
kupalovz <gohisobim@gmail.com>
2026-05-06 09:21:57 +0500

    taqqoslash

--- main.c
+++ main.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
-// LED simulation
-void led_on(void)
+int katta(int a, int b)
 {
-  printf("[LED] ON\n");
+  if (a > b)
+    {
+       return a;
+    }else{
+      return b;
+    }
 }
 
-void led_off(void)
+int main()
 {
-  printf("[LED] OFF\n");
-}
-
-// Checks and warns about temperature
-
-void checks_temperature(int temp) 
-{
-  if (temp > 30)
-  {
-    printf("[WARNING] High temperature: %d C\n", temp);
-    led_on();
-    
-  }else{
-    
-    printf("[OK] Normal temperature: %d C\n", temp);
-    led_off();
-  }
-}
-
-
-int main(void)
-{
- checks_temperature(25);
- checks_temperature(35);
- return 0;
+    printf("%d\n", katta(5.6, 3));
+    printf("%d\n", katta(2, 9));
+    printf("%d\n", katta(4, 4));
+    return 0;
 }
