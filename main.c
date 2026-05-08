c64e97d73629ff2b942e8b8210d84fdab9db68df
kupalovz <gohisobim@gmail.com>
2026-05-08 17:59:46 +0500

    Reverse order

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
-int katta(int a, int b)
-{
-  if (a > b)
-    {
-       return a;
-    }else{
-      return b;
-    }
-}
 
 int main()
 {
-    printf("%d\n", katta(5.6, 3));
-    printf("%d\n", katta(2, 9));
-    printf("%d\n", katta(4, 4));
-    return 0;
+  //massiv 1
+  int numbers1[5] = {10, 20, 30, 40, 50};
+  //massiv 2
+  int numbers2[5] = {34, 17, 89, 42, 55};
+  
+  int big = numbers2[0];
+  int i;
+  
+  printf("Reverse order: ");
+  
+  //Reverse order
+  for(i = 4; i >= 0; i--)
+  {
+    printf("%d ", numbers1[i]);
+  }
+  
+  printf("\n");
+  //Big number
+  for(i = 1; i < 5; i++)
+  {
+    if(numbers2[i] > big)
+    {
+       big = numbers2[i];
+    }
+    
+  }
+  printf("Big num: %d\n", big);
+  
+  return 0;
 }
