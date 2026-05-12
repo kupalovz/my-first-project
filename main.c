c086cf28073d85a268d925b735d1201399f1f219
kupalovz <gohisobim@gmail.com>
2026-05-12 19:02:40 +0500

    7-day. Sensor Temp and Voltage

--- main.c
+++ main.c
@@ -2,37 +2,78 @@
 #include <string.h>
 
 typedef struct {
-  
-  char marka[20];
-  int speed;
-  float oil;
-  
-} Car_t;
+  char model[20];
+  int temperature;
+  float voltage;
+} Sensor_t;
 
-
-void car_info(Car_t m) {
-  
-  printf("Marka : %s\n", m.marka);
-  printf("Speed: %d km/h\n", m.speed);
-  printf("Oil: %.1f litr\n", m.oil);
+void out(Sensor_t *s) {
+  printf("--%s--\n", s -> model);
+  printf("Temperature:%d C\n", s -> temperature);
+  printf("Voltage:%.2f V\n", s -> voltage);
 }
 
-int main(){
+int high_temp(Sensor_t *sensors, int n) {
+  int max_i = 0;
+  int i;
   
-  Car_t characters1;
+  for(i = 1; i < n; i++) {
+    if(sensors[i].temperature > sensors[max_i].temperature) {
+      max_i = i;
+    }
+    
+  }
+  return max_i;
   
-  strcpy(characters1.marka, "Nexia");
-  characters1.speed = 250;
-  characters1.oil = 20.49;
+}
+
+int low_voltage(Sensor_t *sensors, int n) {
+  int min_i = 0;
+  int i;
   
-  Car_t characters2;
+  for(i = 1; i < n; i++) {
+    if(sensors[i].voltage < sensors[min_i].voltage) {
+      min_i = i;
+    }
+  }
+  return min_i;
+}
+
+int main()
+{
+  Sensor_t sensors[3];
   
-  strcpy(characters2.marka, "Cobalt");
-  characters2.speed = 150;
-  characters2.oil = 15.58;
+  // Sensor 1
+  strcpy(sensors[0].model, "DHT11");
+  sensors[0].temperature = 15;
+  sensors[0].voltage = 3.2; 
   
-  car_info(characters1);
-  car_info(characters2);
+  // Sensor 2
+  strcpy(sensors[1].model, "BMP280");
+  sensors[1].temperature = 25;
+  sensors[1].voltage = 4.0;
+  
+  //Sensors 3
+  strcpy(sensors[2].model, "DS18B20");
+  sensors[2].temperature = 20;
+  sensors[2].voltage = 3.0;
+  
+  
+  // Out of all
+  int i;
+  for (i = 0; i < 3; i++) {
+    out(&sensors[i]);
+  } 
+  
+  // Hight temperature
+  int idx = high_temp(sensors, 3);
+  printf("\nHight temperature sensor: %s (%d C)\n",
+  sensors[idx].model, sensors[idx].temperature);
+  
+  // Low voltage
+  int idy = low_voltage(sensors, 3);
+  printf("\nLow voltage sensor: %s (%.1f V)\n",
+  sensors[idy].model, sensors[idy].voltage);
   
   return 0;
 }
